c48de4013b7d2de00e8de2c60caccb5abba56c47
JOU45 <106865417+JOU45@users.noreply.github.com>
2024-06-04 21:24:34 +0200

    er
    gewr

--- script.js
+++ script.js
@@ -1,62 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuBtn = document.querySelector('.menu-btn');
-    const menuContent = document.querySelector('.menu-content');
-    const menuMenuHover = document.querySelector('.menu-hover');
-    const underMenu = document.querySelector('.menu-content-hidden');
-    const closebaton = document.querySelector('.closebaton');
-    const burger = document.querySelector('.burger');
-    const closeBurger = document.querySelector('.menu-menu-closeBurger');
+    const close = document.querySelector('.closebaton'); // мненюшка с керстом или 3 богатыря
+    const nav = document.querySelector('.navbar'); // менюшка мейн
+    const openSVG = document.querySelector('.burger'); // toggle(open)
+    const closeSVG = document.querySelector('.closeSVG'); // toggle(close)
 
-    menuBtn.addEventListener('click', function() {
-        if (menuContent.classList.contains('inactive')) {
-            menuContent.classList.remove('inactive');
-            menuContent.classList.add('active');
-
-        } else {
-            menuContent.classList.remove('active');
-            menuContent.classList.add('inactive');
-        }
+    close.addEventListener('click', function() {
+        close.classList.toggle('close-menu');
+        close.classList.toggle('open-menu');
+        nav.classList.toggle('close');
+        nav.classList.toggle('open');
+        //open-menu
+        closeSVG.classList.toggle('hidden-menu-toggle');
+        openSVG.classList.toggle('hidden-menu-toggle');
     });
-
-    menuMenuHover.addEventListener('mouseover', function() {
-        underMenu.style.animation = 'slideInMenu 2s ease 0s 1 alternate forwards running';
-        underMenu.classList.add('active');
-        underMenu.classList.remove('inactive');
-        console.log('open Menu --> Menu');
-    });
-
-    closebaton.addEventListener('click', function() {
-        underMenu.style.animation = 'slideOutMenu 2s';
-        underMenu.classList.add('inactive');
-        underMenu.classList.remove('active');
-        console.log('close Menu --> Menu')
-
-    });
-
-    burger.addEventListener('click', function() {
-      burger.classList.add('active');
-      closeBurger.classList.add('active');
-      closeBurger.classList.remove('inactive');
-      closeBurger.style.animation = 'slideInMenu 1s ease 0s 1 alternate forwards running';
-      menuContent.classList.add('inactive');
-      menuContent.classList.remove('active');
-    });
-
-    closeBurger.addEventListener('click', function() {
-      burger.classList.remove('active');
-      closeBurger.classList.remove('active');
-      closeBurger.classList.add('inactive');
-      closeBurger.style.animation = 'slideOutMenu 1s ease 0s 1 alternate forwards running';
-      menuContent.classList.add('active');
-      menuContent.classList.remove('inactive');
-      underMenu.style.animation = 'slideOutMenu 0.2s';
-        underMenu.classList.add('inactive');
-        underMenu.classList.remove('active');
-        console.log('close Menu --> Menu')
-
-    });
-
-
-
-
 });
